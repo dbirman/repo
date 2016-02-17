061f7c3b405d2cb8bbc421e3c7c228aef599eb3f
Dan Birman <danbirman@gmail.com>
2016-02-16 23:39:05 -0800

    Adding lots of stuff, deleting other stuff

--- MATLAB/Pipeline/pipeline.m
+++
@@ -1,4 +0,0 @@
-
-%% Run the main pipeline, you can modify this script as necessary.
-
-% Begin by setting up the global variables.
--- MATLAB/generate_simulations.m
+++
@@ -1,9 +0,0 @@
-%% Setup Variables
-
-chans = 10;
-dpoints = 10; % 120ms * 10
-time = 10000; % 10 s
-t = 0; % Threshold height
-
-%% Generate a random classifier sequence
-
